5300177c5c9c7cd0a27355ef690be5ba61481c48
Charith Dilsanka <44514823+RNCDRajanayake@users.noreply.github.com>
2023-11-03 00:24:39 +0530

    Completed

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-alasndljsnjnds
+
+let saveEl = document.getElementById("save-el")
+let countEl = document.getElementById("count-el");
+
+let count = 0;
+
+function increment() {
+  count += 1;
+  countEl.innerText = count;
+  counsole.log(count);
+}
+
+function save() {
+
+    let countStr = count + " - "
+    saveEl.textContent += countStr 
+    countEl.textContent = 0
+    count = 0
+}
+
